9ab553c46ea2444e6bc8886a0900ee5b2de75f7d
deng-ui <enqn_k@naver.com>
2018-11-02 17:24:33 +0900

    score, level add

--- 10/app.js
+++ 10/app.js
@@ -1,17 +1,37 @@
+var warpWidth=330;
+var bugWidth=10;
+
+var score=0;
+var level=1;
+var missing=0;
+
 var box=document.querySelector('.box');
 var moveBox=document.getElementById('box');
+var write=document.getElementById('write');
+var backgroundBox=document.getElementById('backgroundBox');
+
+write.innerHTML=`Score :${score}<br>Level:${level}<br>Missing:${missing}`;
 
 function move(){
     box.style.backgroundColor = 'yellow';
-    var left= Math.floor(Math.random() * 330) + 10;
-    var top= Math.floor(Math.random()*330)+10;
+    var left= Math.floor(Math.random() * warpWidth) + bugWidth;
+    var top= Math.floor(Math.random()*warpWidth)+bugWidth;
     box.style.left=left+'px';
     box.style.top=top+'px';
 }
 
 function remove(){
     box.style.backgroundColor = 'white';
+    score++;
+    write.innerHTML=`Score :${score}<br>Level:${level}<br>Missing:${missing}`;
+    event.stopPropagation();
+}
+
+function wrongClick(){
+    missing++;
+    write.innerHTML=`Score :${score}<br>Level:${level}<br>Missing:${missing}`;
 }
 
 setInterval(move, 1500);
 moveBox.addEventListener('click', remove);
+backgroundBox.addEventListener('click', wrongClick);
